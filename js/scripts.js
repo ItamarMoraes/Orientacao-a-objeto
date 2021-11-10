e4df146ef6fb7bd449e3828f1efb5a913969160a
Itamar <ivoitamar@gmail.com>
2021-11-10 17:07:20 -0300

    Criação do arquivo scripts.js e inicio da criação da classe contaBancaria.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+﻿class contaBancaria {
+    constructor(agencia, numero, tipo, saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = saldo;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+    set saldo(){
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo) {
+            return `Operação negada.`;
+        }
+
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+}
