fd568253990e1ffbd91ffa01eca1e778ef0d3081
Itamar <ivoitamar@gmail.com>
2021-11-11 11:39:54 -0300

    Correção de erros.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 ﻿class ContaBancaria {
-    constructor(agencia, numero, tipo, saldo) {
+    constructor(agencia, numero, tipo) {
         this.agencia = agencia;
         this.numero = numero;
         this.tipo = tipo;
-        this._saldo = saldo;
+        this._saldo = 0;
     }
 
     get saldo(){
         return this._saldo;
     }
 
-    set saldo(){
+    set saldo(valor){
         this._saldo = valor;
     }
 
@@ -33,34 +33,34 @@
 
 class ContaCorrente extends ContaBancaria {
     constructor(agencia, numero, saldo, cartaoCredito){
-        super(agencia, numero, saldo);
+        super(agencia, numero);
         this.tipo = 'corrente';
-        this.cartaoCredito = cartaoCredito;
+        this._cartaoCredito = cartaoCredito;
     }
 
-    get this._cartaoCredito(valor) {
-        this.this._cartaoCredito = valor;
+    get cartaoCredito() {
+        return this._cartaoCredito;
     }
 }
 
 class ContaPoupanca extends ContaBancaria {
-    constructor(agencia, numero, saldo){
-        super(agencia, numero, saldo);
+    constructor(agencia, numero){
+        super(agencia, numero);
         this.tipo = 'poupança';
     }
 }
 
 class ContaUniversitaria extends ContaBancaria {
-    constructor(agencia, numero, saldo){
-        super(agencia, numero, saldo);
+    constructor(agencia, numero){
+        super(agencia, numero);
         this.tipo = 'universitária';
     }
 
     sacar(valor) {
         if(valor > 500) {
             return "Operação negada";
-
-            this._saldo = this._saldo - valor;
         }
+
+        this._saldo = this._saldo - valor;
     }
 }
